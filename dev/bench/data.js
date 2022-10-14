6dd2cf13c90b7a516eb805d706a153f84ef8c7d9
github-action-benchmark <github@users.noreply.github.com>
2022-10-14 09:45:39 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 6c5611f8da8a589b9c51e82fddd68386ad8f996d

---
+++ dev/bench/data.js
@@ -0,0 +1,53 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1665740738971,
+  "repoUrl": "https://github.com/matthewkenzie/FCCAnalyses",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "matthew.william.kenzie@cern.ch",
+            "name": "Matthew Kenzie",
+            "username": "matthewkenzie"
+          },
+          "committer": {
+            "email": "matthew.william.kenzie@cern.ch",
+            "name": "Matthew Kenzie",
+            "username": "matthewkenzie"
+          },
+          "distinct": true,
+          "id": "6c5611f8da8a589b9c51e82fddd68386ad8f996d",
+          "message": "Mods for running stage2 of the analysis\n\nAdditions for making files and training the stage2 MVA.\nCan also make the background splines for efficiency and cut\noptimisation.",
+          "timestamp": "2022-10-14T10:34:20+01:00",
+          "tree_id": "c51e622ee5d18a7f422624ba71e18cf09749907e",
+          "url": "https://github.com/matthewkenzie/FCCAnalyses/commit/6c5611f8da8a589b9c51e82fddd68386ad8f996d"
+        },
+        "date": 1665740737809,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 14.57982087135315,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 19.589550971984863,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/test",
+            "value": 14.380913734436035,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      }
+    ]
+  }
+}
