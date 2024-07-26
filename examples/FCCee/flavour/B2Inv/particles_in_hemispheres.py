dc279fb7be780570dd542be24ca1fffaa6e0b073
Ritwik Mangrulkar <rrm42@cam.ac.uk>
2024-07-26 20:01:18 +0100

    Minor update to electron energy plotter and fix bug in particle counter which removed zeros

--- examples/FCCee/flavour/B2Inv/particles_in_hemispheres.py
+++ examples/FCCee/flavour/B2Inv/particles_in_hemispheres.py
@@ -8,220 +8,234 @@ import config as cfg
 
 plt.style.use('fcc.mplstyle')
 
+def particle_counter(file, cosexpr, cut):
+    min_k = np.array([])
+    min_p = np.array([])
+    min_e = np.array([])
+    min_m = np.array([])
+    min_t = np.array([])
+    max_k = np.array([])
+    max_p = np.array([])
+    max_e = np.array([])
+    max_m = np.array([])
+    max_t = np.array([])
+    min_lep = np.array([])
+    max_lep = np.array([])
+    
+    cut_min_k = np.array([])
+    cut_min_p = np.array([])
+    cut_min_e = np.array([])
+    cut_min_m = np.array([])
+    cut_min_t = np.array([])
+    cut_max_k = np.array([])
+    cut_max_p = np.array([])
+    cut_max_e = np.array([])
+    cut_max_m = np.array([])
+    cut_max_t = np.array([])
+    cut_min_lep = np.array([])
+    cut_max_lep = np.array([])
+    
+    if cut:
+
+        for data in uproot.iterate(file+':events', expressions=['cos', 'pid', 'Emin'], aliases={'cos':cosexpr, 'Emin':'EVT_Thrust_Emin_e', 'pid':'abs(MC_PDG)'}):
+            
+            # Particle count with Emin cut
+            #min_k = np.append( min_k , ak.count(data['pid'][(data['Emin'] < 20) & (data['cos'] > 0) & ((data['pid'] == 311) | (data['pid']) == 321)], axis=-1).to_numpy() )
+            #max_k = np.append( max_k , ak.count(data['pid'][(data['Emin'] < 20) & (data['cos'] < 0) & ((data['pid'] == 311) | (data['pid']) == 321)], axis=-1).to_numpy() )
+            #min_p = np.append( min_p , ak.count(data['pid'][(data['Emin'] < 20) & (data['cos'] > 0) & ((data['pid'] == 111) | (data['pid']) == 211)], axis=-1).to_numpy() )
+            #max_p = np.append( max_p , ak.count(data['pid'][(data['Emin'] < 20) & (data['cos'] < 0) & ((data['pid'] == 111) | (data['pid']) == 121)], axis=-1).to_numpy() )
+            min_k = np.append( min_k , 
+                    ak.count(data['pid'][(data['Emin'] < 20) & (data['cos'] > 0) & (data['pid'] == 311)], axis=-1).to_numpy() + 
+                    ak.count(data['pid'][(data['Emin'] < 20) & (data['cos'] > 0) & (data['pid'] == 321)], axis=-1).to_numpy() )
+            max_k = np.append( max_k , 
+                    ak.count(data['pid'][(data['Emin'] < 20) & (data['cos'] < 0) & (data['pid'] == 311)], axis=-1).to_numpy() + 
+                    ak.count(data['pid'][(data['Emin'] < 20) & (data['cos'] < 0) & (data['pid'] == 321)], axis=-1).to_numpy() )
+            min_p = np.append( min_p , 
+                    ak.count(data['pid'][(data['Emin'] < 20) & (data['cos'] > 0) & (data['pid'] == 111)], axis=-1).to_numpy() +
+                    ak.count(data['pid'][(data['Emin'] < 20) & (data['cos'] > 0) & (data['pid'] == 211)], axis=-1).to_numpy() )
+            max_p = np.append( max_p , 
+                    ak.count(data['pid'][(data['Emin'] < 20) & (data['cos'] < 0) & (data['pid'] == 111)], axis=-1).to_numpy() +
+                    ak.count(data['pid'][(data['Emin'] < 20) & (data['cos'] < 0) & (data['pid'] == 211)], axis=-1).to_numpy() )
+
+
+            min_e = np.append( min_e , ak.count(data['pid'][(data['Emin']  < 20) & (data['cos'] > 0) & (data['pid'] == 11)], axis=-1).to_numpy() )
+            max_e = np.append( max_e , ak.count(data['pid'][(data['Emin']  < 20) & (data['cos'] < 0) & (data['pid'] == 11)], axis=-1).to_numpy() )
+            min_m = np.append( min_m , ak.count(data['pid'][(data['Emin']  < 20) & (data['cos'] > 0) & (data['pid'] == 13)], axis=-1).to_numpy() )
+            max_m = np.append( max_m , ak.count(data['pid'][(data['Emin']  < 20) & (data['cos'] < 0) & (data['pid'] == 13)], axis=-1).to_numpy() ) 
+            min_t = np.append( min_t , ak.count(data['pid'][(data['Emin']  < 20) & (data['cos'] > 0) & (data['pid'] == 15)], axis=-1).to_numpy() )
+            max_t = np.append( max_t , ak.count(data['pid'][(data['Emin']  < 20) & (data['cos'] < 0) & (data['pid'] == 15)], axis=-1).to_numpy() )
+        
+            min_lep = np.append( min_lep , min_e + min_m + min_t )
+            max_lep = np.append( max_lep , max_e + max_m + max_t )
+
+    else:
+        for data in uproot.iterate(file+':events', expressions=['cos', 'pid'], aliases={'cos':cosexpr, 'pid':'abs(MC_PDG)'}):
+        
+            # Particle count without Emin cut
+            #min_k = np.append( min_k , ak.count(data['pid'][(data['cos'] > 0) & ((data['pid'] == 311) | (data['pid']) == 321)], axis=-1).to_numpy() )
+            #max_k = np.append( max_k , ak.count(data['pid'][(data['cos'] < 0) & ((data['pid'] == 311) | (data['pid']) == 321)], axis=-1).to_numpy() )
+            #min_p = np.append( min_p , ak.count(data['pid'][(data['cos'] > 0) & ((data['pid'] == 111) | (data['pid']) == 211)], axis=-1).to_numpy() )
+            #max_p = np.append( max_p , ak.count(data['pid'][(data['cos'] < 0) & ((data['pid'] == 111) | (data['pid']) == 121)], axis=-1).to_numpy() )
+            min_k = np.append( min_k , 
+                    ak.count(data['pid'][(data['cos'] > 0) & (data['pid'] == 311)], axis=-1).to_numpy() + 
+                    ak.count(data['pid'][(data['cos'] > 0) & (data['pid'] == 321)], axis=-1).to_numpy() )
+            max_k = np.append( max_k , 
+                    ak.count(data['pid'][(data['cos'] < 0) & (data['pid'] == 311)], axis=-1).to_numpy() + 
+                    ak.count(data['pid'][(data['cos'] < 0) & (data['pid'] == 321)], axis=-1).to_numpy() )
+            min_p = np.append( min_p , 
+                    ak.count(data['pid'][(data['cos'] > 0) & (data['pid'] == 111)], axis=-1).to_numpy() + 
+                    ak.count(data['pid'][(data['cos'] < 0) & (data['pid'] == 121)], axis=-1).to_numpy() )
+            max_p = np.append( max_p , 
+                    ak.count(data['pid'][(data['cos'] < 0) & (data['pid'] == 111)], axis=-1).to_numpy() + 
+                    ak.count(data['pid'][(data['cos'] < 0) & (data['pid'] == 211)], axis=-1).to_numpy() )
+
+            min_e = np.append( min_e , ak.count(data['pid'][(data['cos'] > 0) & (data['pid'] == 11)], axis=-1).to_numpy() )
+            max_e = np.append( max_e , ak.count(data['pid'][(data['cos'] < 0) & (data['pid'] == 11)], axis=-1).to_numpy() )
+            min_m = np.append( min_m , ak.count(data['pid'][(data['cos'] > 0) & (data['pid'] == 13)], axis=-1).to_numpy() )
+            max_m = np.append( max_m , ak.count(data['pid'][(data['cos'] < 0) & (data['pid'] == 13)], axis=-1).to_numpy() ) 
+            min_t = np.append( min_t , ak.count(data['pid'][(data['cos'] > 0) & (data['pid'] == 15)], axis=-1).to_numpy() )
+            max_t = np.append( max_t , ak.count(data['pid'][(data['cos'] < 0) & (data['pid'] == 15)], axis=-1).to_numpy() )
+        
+            min_lep = np.append( min_lep , min_e + min_m + min_t )
+            max_lep = np.append( max_lep , max_e + max_m + max_t )
+
+    return {'min_k': min_k, 
+            'max_k': max_k, 
+            'min_p': min_p, 
+            'max_p': max_p, 
+            'min_e': min_e, 
+            'max_e': max_e, 
+            'min_m': min_m, 
+            'max_m': max_m, 
+            'min_t': min_t, 
+            'max_t': max_t, 
+            'min_lep': min_lep, 
+            'max_lep': max_lep}
+
+# Signal arrays
 filepath = '/r01/lhcb/mkenzie/fcc/B2Inv/stage0/'
+cosexpr = '(MC_px*EVT_Thrust_x + MC_py*EVT_Thrust_y + MC_pz*EVT_Thrust_z)/(MC_p*(EVT_Thrust_x**2 + EVT_Thrust_y**2 + EVT_Thrust_z**2)**0.5)'
 
-keys = ['MC_PDG']
+sigfile = os.path.join(filepath, cfg.samples[0], '*.root')
+sigdict_nocut = particle_counter(sigfile, cosexpr, False)
+print("Signal files without cut done")
+sigdict_Emcut = particle_counter(sigfile, cosexpr, True)
+print("Signal files with cut done")
 
-# Cut for Emin and particles in min hemisphere
-#cut_min = '(EVT_Thrust_Emin_e < 20) & ((MC_px*EVT_Thrust_x + MC_py*EVT_Thrust_y + MC_pz*EVT_Thrust_z)/((MC_px**2 + MC_py**2 + MC_pz**2)**0.5*(EVT_Thrust_x**2 + EVT_Thrust_y**2 + EVT_Thrust_z**2)**0.5) > 0)'
+bbfile = os.path.join(filepath, cfg.samples[1], '*.root')
+bbdict_nocut = particle_counter(bbfile, cosexpr, False)
+print("Background files without cut done")
+bbdict_Emcut = particle_counter(bbfile, cosexpr, True)
+print("Background files with cut done")
 
-# Without cut
-cut_min = '(MC_px*EVT_Thrust_x + MC_py*EVT_Thrust_y + MC_pz*EVT_Thrust_z)/((MC_px**2 + MC_py**2 + MC_pz**2)**0.5*(EVT_Thrust_x**2 + EVT_Thrust_y**2 + EVT_Thrust_z**2)**0.5) > 0'
 
-# With cut
-#cut_max = '(EVT_Thrust_Emin_e < 20) & ((MC_px*EVT_Thrust_x + MC_py*EVT_Thrust_y + MC_pz*EVT_Thrust_z)/((MC_px**2 + MC_py**2 + MC_pz**2)**0.5*(EVT_Thrust_x**2 + EVT_Thrust_y**2 + EVT_Thrust_z**2)**0.5) < 0)'
+###################### Plotting #############################
 
-# Without cut
-cut_max = '(MC_px*EVT_Thrust_x + MC_py*EVT_Thrust_y + MC_pz*EVT_Thrust_z)/((MC_px**2 + MC_py**2 + MC_pz**2)**0.5*(EVT_Thrust_x**2 + EVT_Thrust_y**2 + EVT_Thrust_z**2)**0.5) < 0'
-file = os.path.join(filepath, cfg.samples[4], 'chunk_0.root')
-
-with uproot.open(file+':events') as tree:
-    
-    data_min = uproot.concatenate(tree, expressions=keys, cut = cut_min)
-    min_ka_count = ak.count(data_min['MC_PDG'][(abs(data_min['MC_PDG']) == 311) | (abs(data_min['MC_PDG']) == 321)], axis=-1, mask_identity=True)
-    min_pi_count = ak.count(data_min['MC_PDG'][(abs(data_min['MC_PDG']) == 111) | (abs(data_min['MC_PDG']) == 211)], axis=-1, mask_identity=True)
-    min_ep_count = ak.count(data_min['MC_PDG'][abs(data_min['MC_PDG']) == 11], axis=-1, mask_identity=True)
-    min_mu_count = ak.count(data_min['MC_PDG'][abs(data_min['MC_PDG']) == 13], axis=-1, mask_identity=True)
-    min_ta_count = ak.count(data_min['MC_PDG'][abs(data_min['MC_PDG']) == 15], axis=-1, mask_identity=True)
-    
-    data_max = uproot.concatenate(tree, expressions=keys, cut = cut_max)
-    max_ka_count = ak.count(data_max['MC_PDG'][(abs(data_max['MC_PDG']) == 311) | (abs(data_max['MC_PDG']) == 321)], axis=-1, mask_identity=True)
-    max_pi_count = ak.count(data_max['MC_PDG'][(abs(data_max['MC_PDG']) == 111) | (abs(data_max['MC_PDG']) == 211)], axis=-1, mask_identity=True)
-    max_ep_count = ak.count(data_max['MC_PDG'][abs(data_max['MC_PDG']) == 11], axis=-1, mask_identity=True)
-    max_mu_count = ak.count(data_max['MC_PDG'][abs(data_max['MC_PDG']) == 13], axis=-1, mask_identity=True)
-    max_ta_count = ak.count(data_max['MC_PDG'][abs(data_max['MC_PDG']) == 15], axis=-1, mask_identity=True)
-
-#min_ka_count = ak.to_numpy(min_ka_count, allow_missing=True)
-#min_pi_count = ak.to_numpy(min_pi_count, allow_missing=True)
-#min_ep_count = ak.to_numpy(min_ep_count, allow_missing=True)
-#min_mu_count = ak.to_numpy(min_mu_count, allow_missing=True)
-#min_ta_count = ak.to_numpy(min_ta_count, allow_missing=True)
-#max_ka_count = ak.to_numpy(max_ka_count, allow_missing=True)
-#max_pi_count = ak.to_numpy(max_pi_count, allow_missing=True)
-#max_ep_count = ak.to_numpy(max_ep_count, allow_missing=True)
-#max_mu_count = ak.to_numpy(max_mu_count, allow_missing=True)
-#max_ta_count = ak.to_numpy(max_ta_count, allow_missing=True)
-
-min_tot_lp_count = min_ep_count + min_mu_count + min_ta_count
-max_tot_lp_count = max_ep_count + max_mu_count + max_ta_count
-
-min_ka_count = ak.flatten(min_ka_count, axis=0)
-min_pi_count = ak.flatten(min_pi_count, axis=0)
-min_ep_count = ak.flatten(min_ep_count, axis=0)
-min_mu_count = ak.flatten(min_mu_count, axis=0)
-min_ta_count = ak.flatten(min_ta_count, axis=0)
-max_ka_count = ak.flatten(max_ka_count, axis=0)
-max_pi_count = ak.flatten(max_pi_count, axis=0)
-max_ep_count = ak.flatten(max_ep_count, axis=0)
-max_mu_count = ak.flatten(max_mu_count, axis=0)
-max_ta_count = ak.flatten(max_ta_count, axis=0)
-
-min_tot_lp_count = ak.flatten(min_tot_lp_count, axis=0)
-max_tot_lp_count = ak.flatten(max_tot_lp_count, axis=0)
+titledict = {'k': r"Number of Kaons ($K^0/\bar{K}^0$ or $K^\pm$)",
+             'p': r"Number of Pions ($\pi^0$ or $\pi^\pm$)",
+             'e': r"Number of $e^\pm$",
+             'm': r"Number of $\mu^\pm$",
+             't': r"Number of $\tau^\pm$",
+             'lep': r"Total number of charged leptons"}
 
 min_histopts = {
         'histtype': 'step',
-        'lw': 1.5,
+        'lw': 2,
         'color': 'blue',
         'hatch': '////'}
 
 max_histopts = {
         'histtype': 'step',
-        'lw': 1.5,
+        'lw': 2,
         'color': 'red',
         'hatch': '\\'}
 
-fig, ax = plt.subplots()
+bbmin_histopts = {
+        'histtype': 'stepfilled',
+        'lw': 1,
+        'fill': True,
+        'color': 'orange',
+        'alpha': 0.4}
 
-ax.hist(
-        x = min_ka_count,
-        bins=range(min(min_ka_count), max(min_ka_count) + 1, 1),
-        density = False,
-        label = r'Emin hemisphere',
-        **min_histopts)
+bbmax_histopts = {
+        'histtype': 'stepfilled',
+        'lw': 1,
+        'fill': True,
+        'color': 'green',
+        'alpha': 0.4}
 
-ax.hist(
-        x = max_ka_count,
-        bins=range(min(max_ka_count), max(max_ka_count) + 1, 1),
-        density = False,
-        label = r'Emax hemisphere',
-        **max_histopts)
+for i in titledict:
 
-ax.set_xlabel('Number of particles')
-ax.set_xlim((0, 20))
-ax.set_ylabel('Occurrences')
-ax.set_title('Kaon count in both hemispheres')
-ax.legend()
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+    
+    ax[0].hist(
+            x = sigdict_nocut['min_'+i],
+            bins=np.arange(min(sigdict_nocut['min_'+i]), max(sigdict_nocut['min_'+i]) + 1, 1),
+            density = False,
+            label = r"$B_s^0 \to \nu\bar{\nu}$: Emin",
+            **min_histopts)
+    
+    ax[0].hist(
+            x = sigdict_nocut['max_'+i],
+            bins=np.arange(min(sigdict_nocut['max_'+i]), max(sigdict_nocut['max_'+i]) + 1, 1),
+            density = False,
+            label = r"$B_s^0 \to \nu\bar{\nu}$: Emax",
+            **max_histopts)
 
-plt.savefig('/usera/rrm42/private/fcc-figures/part-count/signal_k.png')
+    ax[0].hist(
+            x = bbdict_nocut['min_'+i],
+            bins=np.arange(min(bbdict_nocut['min_'+i]), max(bbdict_nocut['min_'+i]) + 1, 1),
+            density = False,
+            label = r"$Z \to b\bar{b}$: Emin",
+            **bbmin_histopts)
 
-fig, ax = plt.subplots()
+    ax[0].hist(
+            x = bbdict_nocut['max_'+i],
+            bins=np.arange(min(bbdict_nocut['max_'+i]), max(bbdict_nocut['max_'+i]) + 1, 1),
+            density = False,
+            label = r"$Z \to b\bar{b}$: Emax",
+            **bbmax_histopts)
 
-ax.hist(
-        x = min_pi_count,
-        bins=range(min(min_pi_count), max(min_pi_count) + 1, 1),
-        density = False,
-        label = r'Emin hemisphere',
-        **min_histopts)
+    ax[0].set_ylabel('Total occurrences')
+    ax[0].set_xlabel('Count')
+    ax[0].set_title('Without cut')
+    ax[0].tick_params(labelsize=8)
+    ax[0].legend()
 
-ax.hist(
-        x = max_pi_count,
-        bins=range(min(max_pi_count), max(max_pi_count) + 1, 1),
-        density = False,
-        label = r'Emax hemisphere',
-        **max_histopts)
+    ax[1].hist(
+            x = sigdict_Emcut['min_'+i],
+            bins=np.arange(min(sigdict_Emcut['min_'+i]), max(sigdict_Emcut['min_'+i]) + 1, 1),
+            density = False,
+            label = r"$B_s^0 \to \nu\bar{\nu}$: Emin",
+            **min_histopts)
+    
+    ax[1].hist(
+            x = sigdict_Emcut['max_'+i],
+            bins=np.arange(min(sigdict_Emcut['max_'+i]), max(sigdict_Emcut['max_'+i]) + 1, 1),
+            density = False,
+            label = r"$B_s^0 \to \nu\bar{\nu}$: Emax",
+            **max_histopts)
 
-ax.set_xlabel('Number of particles')
-ax.set_xlim((0, 40))
-ax.set_ylabel('Occurrences')
-ax.set_title('Pion count in both hemispheres')
-ax.legend()
+    ax[1].hist(
+            x = bbdict_Emcut['min_'+i],
+            bins=np.arange(min(bbdict_Emcut['min_'+i]), max(bbdict_Emcut['min_'+i]) + 1, 1),
+            density = False,
+            label = r"$Z \to b\bar{b}$: Emin",
+            **bbmin_histopts)
 
-plt.savefig('/usera/rrm42/private/fcc-figures/part-count/signal_pi.png')
+    ax[1].hist(
+            x = bbdict_Emcut['max_'+i],
+            bins=np.arange(min(bbdict_Emcut['max_'+i]), max(bbdict_Emcut['max_'+i]) + 1, 1),
+            density = False,
+            label = r"$Z \to b\bar{b}$: Emax",
+            **bbmax_histopts)
 
-fig, ax = plt.subplots()
+    ax[1].set_ylabel('Total occurrences')
+    ax[1].set_xlabel('Count')
+    ax[1].set_title('With Emin cut')
+    ax[1].tick_params(labelsize=8)
+    ax[1].legend()
 
-ax.hist(
-        x = min_ep_count,
-        bins=range(min(min_ep_count), max(min_ep_count) + 1, 1),
-        density = False,
-        label = r'Emin hemisphere',
-        **min_histopts)
-
-ax.hist(
-        x = max_ep_count,
-        bins=range(min(max_pi_count), max(max_ep_count) + 1, 1),
-        density = False,
-        label = r'Emax hemisphere',
-        **max_histopts)
-
-ax.set_xlabel('Number of particles')
-ax.set_xlim((0, 10))
-ax.set_ylabel('Occurrences')
-ax.set_title(r'$e^+e^-$ count in both hemispheres')
-ax.legend()
-
-plt.savefig('/usera/rrm42/private/fcc-figures/part-count/signal_ep.png')
-
-fig, ax = plt.subplots()
-
-ax.hist(
-        x = min_mu_count,
-        bins=range(min(min_mu_count), max(min_mu_count) + 1, 1),
-        density = False,
-        label = r'Emin hemisphere',
-        **min_histopts)
-
-ax.hist(
-        x = max_mu_count,
-        bins=range(min(max_mu_count), max(max_mu_count) + 1, 1),
-        density = False,
-        label = r'Emax hemisphere',
-        **max_histopts)
-
-ax.set_xlabel('Number of particles')
-ax.set_xlim((0, 5))
-ax.set_ylabel('Occurrences')
-ax.set_title(r'$\mu^+\mu^-$ count in both hemispheres')
-ax.legend()
-
-plt.savefig('/usera/rrm42/private/fcc-figures/part-count/signal_mu.png')
-
-fig, ax = plt.subplots()
-
-ax.hist(
-        x = min_ta_count,
-        bins=range(min(min_ta_count), max(min_ta_count) + 1, 1),
-        density = False,
-        label = r'Emin hemisphere',
-        **min_histopts)
-
-ax.hist(
-        x = max_ta_count,
-        bins=range(min(max_ta_count), max(max_ta_count) + 1, 1),
-        density = False,
-        label = r'Emax hemisphere',
-        **max_histopts)
-
-ax.set_xlabel('Number of particles')
-ax.set_xlim((0, 5))
-ax.set_ylabel('Occurrences')
-ax.set_title(r'$\tau^+\tau^-$ count in both hemispheres')
-ax.legend()
-
-plt.savefig('/usera/rrm42/private/fcc-figures/part-count/signal_ta.png')
-
-fig, ax = plt.subplots()
-
-ax.hist(
-        x = min_tot_lp_count,
-        bins=range(min(min_tot_lp_count), max(min_tot_lp_count) + 1, 1),
-        density = False,
-        label = r'Emin hemisphere',
-        **min_histopts)
-
-ax.hist(
-        x = max_tot_lp_count,
-        bins=range(min(max_tot_lp_count), max(max_tot_lp_count) + 1, 1),
-        density = False,
-        label = r'Emax hemisphere',
-        **max_histopts)
-
-ax.set_xlabel('Number of particles')
-ax.set_xlim((0, 10))
-ax.set_ylabel('Occurrences')
-ax.set_title('Total lepton count in both hemispheres')
-ax.legend()
-
-plt.savefig('/usera/rrm42/private/fcc-figures/part-count/signal_lepton.png')
-
+    fig.suptitle(titledict[i])
+    fig.tight_layout()
+    plt.savefig('/r01/lhcb/rrm42/fcc/'+i+'.png', dpi=300)
