4f4b7f9da52f73387c58f60c02bb1a166efb3b56
Ritwik Mangrulkar <rrm42@cam.ac.uk>
2024-08-29 13:23:59 +0100

    Make vertex tester use the working directory instead of an absolute path to configtest.py

--- examples/FCCee/flavour/B2Inv/newvertexfittertest.py
+++ examples/FCCee/flavour/B2Inv/newvertexfittertest.py
@@ -2,7 +2,7 @@ import sys
 import os
 #import ROOT
 from yaml import YAMLError, safe_load
-sys.path.append('/r02/lhcb/rrm42/fcc/FCCAnalyses/examples/FCCee/flavour/B2Inv')
+sys.path.append(os.getcwd())
 import configtest as cfg
 
 #Mandatory: List of processes
@@ -12,7 +12,7 @@ processList = cfg.processList
 prodTag   = cfg.fccana_opts['prodTag']
 
 #Optional: output directory, default is local running directory
-outputDir = cfg.fccana_opts['outputDir'] 
+outputDir = os.getcwd()
 
 #Optional: analysisName, default is ""
 #analysisName = cfg.fccana_opts['analysisName']
@@ -54,22 +54,22 @@ class RDFanalysis():
             ## Test
             ##############################
             #### Current method -- Franco Bedeschi, VertexFitterSimple
-            #.Define("MC_VertexObject",          "myUtils::get_MCVertexObject(Particle, ParticleParents)")
-            #.Define("Rec_PrimaryTracks",        "VertexFitterSimple::get_PrimaryTracks(EFlowTrack_1, true, 4.5, 20e-3, 300, 0., 0., 0.)")
-            #.Define("Rec_PrimaryVertexObject",  "VertexFitterSimple::VertexFitter_Tk( 1, Rec_PrimaryTracks, true, 4.5, 20e-3, 300 )")
-            #.Define("Rec_PrimaryVertex",        "Rec_PrimaryVertexObject.vertex")
-            #.Define("Rec_VertexObject",         "myUtils::get_VertexObject(MC_VertexObject, ReconstructedParticles, EFlowTrack_1, MCRecoAssociationsRec, MCRecoAssociationsGen)")
+            .Define("MC_VertexObject",          "myUtils::get_MCVertexObject(Particle, ParticleParents)")
+            .Define("Rec_PrimaryTracks",        "VertexFitterSimple::get_PrimaryTracks(EFlowTrack_1, true, 4.5, 20e-3, 300, 0., 0., 0.)")
+            .Define("Rec_PrimaryVertexObject",  "VertexFitterSimple::VertexFitter_Tk( 1, Rec_PrimaryTracks, true, 4.5, 20e-3, 300 )")
+            .Define("Rec_PrimaryVertex",        "Rec_PrimaryVertexObject.vertex")
+            .Define("Rec_VertexObject",         "myUtils::get_VertexObject(MC_VertexObject, ReconstructedParticles, EFlowTrack_1, MCRecoAssociationsRec, MCRecoAssociationsGen)")
 
-            #.Define("Rec_PV_x",                 "Rec_PrimaryVertex.position.x")
-            #.Define("Rec_PV_y",                 "Rec_PrimaryVertex.position.y")
-            #.Define("Rec_PV_z",                 "Rec_PrimaryVertex.position.z")
-            #.Define("Rec_vtx_n",                "Rec_VertexObject.size()")
-            #.Define("Rec_vtx_x",                "myUtils::get_Vertex_x(Rec_VertexObject)")
-            #.Define("Rec_vtx_y",                "myUtils::get_Vertex_y(Rec_VertexObject)")
-            #.Define("Rec_vtx_z",                "myUtils::get_Vertex_z(Rec_VertexObject)")
+            .Define("Rec_PV_x",                 "Rec_PrimaryVertex.position.x")
+            .Define("Rec_PV_y",                 "Rec_PrimaryVertex.position.y")
+            .Define("Rec_PV_z",                 "Rec_PrimaryVertex.position.z")
+            .Define("Rec_vtx_n",                "Rec_VertexObject.size()")
+            .Define("Rec_vtx_x",                "myUtils::get_Vertex_x(Rec_VertexObject)")
+            .Define("Rec_vtx_y",                "myUtils::get_Vertex_y(Rec_VertexObject)")
+            .Define("Rec_vtx_z",                "myUtils::get_Vertex_z(Rec_VertexObject)")
 
             #### Test method -- ACTS, VertexFinderActs or VertexFitterActs           
-            .Define("Rec_VertexObjectActs",     "VertexFinderActs::VertexFinderAMVF( EFlowTrack_1 )")
+            #.Define("Rec_VertexObjectActs",     "VertexFinderActs::VertexFinderAMVF( EFlowTrack_1 )")
             #.Define("Rec_vtx_n",                "Rec_VertexObjectActs.size()")
             #.Define("Rec_vtx_x",                "myUtils::get_Vertex_x(Rec_VertexObjectActs)")
             #.Define("Rec_vtx_y",                "myUtils::get_Vertex_y(Rec_VertexObjectActs)")
@@ -85,13 +85,13 @@ class RDFanalysis():
             "MC_PDG",
             "MC_e",
             "MC_m",
-            #"Rec_PV_x",
-            #"Rec_PV_y",
-            #"Rec_PV_z",
-            #"Rec_vtx_n",
-            #"Rec_vtx_x",
-            #"Rec_vtx_y",
-            #"Rec_vtx_z",
+            "Rec_PV_x",
+            "Rec_PV_y",
+            "Rec_PV_z",
+            "Rec_vtx_n",
+            "Rec_vtx_x",
+            "Rec_vtx_y",
+            "Rec_vtx_z",
         ]
 
         return branchList
